01f46386684fdfcc47af64dab7e89dccda974075
phil_not_funny <schrenk.philip@hotmail.com>
2023-02-22 08:27:02 +0100

    feat: added example gui

---
+++ src/main/java/com/pnf/fabric/coordsscreen/misc/TestEnum.java
@@ -0,0 +1,49 @@
+package com.pnf.fabric.coordsscreen.misc;
+
+import dev.lambdaurora.spruceui.util.Nameable;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents a dummy enum.
+ *
+ * @author LambdAurora
+ */
+public enum TestEnum implements Nameable {
+    FIRST,
+    SECOND,
+    THIRD,
+    ANOTHER_VALUE;
+
+    private final Text text;
+
+    TestEnum() {
+        this.text = Text.literal(this.getName());
+    }
+
+    /**
+     * Returns the next enum value available.
+     *
+     * @return The next available enum value.
+     */
+    public @NotNull TestEnum next() {
+        var v = values();
+        if (v.length == this.ordinal() + 1)
+            return v[0];
+        return v[this.ordinal() + 1];
+    }
+
+    /**
+     * Gets the text of this enum value.
+     *
+     * @return The text of this enum value.
+     */
+    public @NotNull Text getText() {
+        return this.text;
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return this.name().toLowerCase();
+    }
+}
